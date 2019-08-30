7d925e1dcc11f87d546eb85de0ddc9d87d8d0b5b
Mohan Kumar <MohanKumarYum@Gmail.com>
2019-08-30 04:39:43 +0000

    Added blog, about & contact pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,14 @@
 import React from "react"
 
-export default () => <div>The Great Gatsby Bootcamp</div>
+// export default () => <div>The Great Gatsby Bootcamp</div>
+
+const IndexPage = () => {
+  return (
+      <div>
+          <h1>Hello.</h1>
+          <h2>I'm Mohan, a full-stack developer living in beautiful Sydney.</h2>
+      </div>
+  );
+};
+
+export default IndexPage;
